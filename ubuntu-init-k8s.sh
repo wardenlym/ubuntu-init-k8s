03a97df5be419e93772861785c77cf4092ff338f
wardenlym <wardenlym@gmail.com>
2021-03-31 11:37:33 +0800

    add sysctl and modprobe

--- ubuntu-init-k8s.sh
+++ ubuntu-init-k8s.sh
@@ -82,14 +82,36 @@ msg "- flag: ${flag}"
 msg "- param: ${param}"
 msg "- arguments: ${args[*]-}"
 
-apt_update() {
-  apt update
+
+set_localtime() {
+  ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 }
 
 apt_install_base_util() {
-  apt install -y git wget curl zsh jq ansible \
-                    net-tools bridge-utils traceroute \
-                    ntp ipvsadm
+  apt update && apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    git \
+    wget \
+    curl \
+    bash-completion \
+    zsh \
+    jq \
+    ansible \
+    unzip \
+    sysstat \
+    dnsutils \
+    tcpdump \
+    telnet \
+    lsof \
+    htop \
+    net-tools \
+    bridge-utils \
+    traceroute \
+    conntrack \
+    ntp \
+    ipvsadm \
+    ipset 
 }
 
 remove_docker() {
@@ -162,8 +184,100 @@ apt_install_kubeadm_kubectl() {
   apt install -y kubelet=1.19.7-00 kubeadm=1.19.7-00 kubectl=1.19.7-00
 }
 
-#apt_update
-#apt_install_base_util
+disable_swap() {
+  swapoff -a && sysctl -w vm.swappiness=0
+  sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
+
+  # 两种写法都可以
+  #sed -ri '/^[^#]*swap/s@^@#@' /etc/fstab
+}
+
+load_module_rke_checks() {
+  rm -f /etc/modules-load.d/rke.conf
+  for module in br_netfilter ip6_udp_tunnel ip_set ip_set_hash_ip ip_set_hash_net iptable_filter iptable_nat iptable_mangle iptable_raw nf_conntrack_netlink nf_conntrack nf_defrag_ipv4 nf_nat nfnetlink udp_tunnel veth vxlan x_tables xt_addrtype xt_conntrack xt_comment xt_mark xt_multiport xt_nat xt_recent xt_set  xt_statistic xt_tcpudp;
+  do
+    modprobe $module
+    /sbin/modinfo -F filename $module |& grep -qv ERROR && modprobe -- $module && echo $module >> /etc/modules-load.d/rke.conf || :
+  done
+  systemctl daemon-reload
+  systemctl enable --now systemd-modules-load.service
+}
+
+load_module_ipvs() {
+  module=(
+  ip_vs
+  ip_vs_rr
+  ip_vs_wrr
+  ip_vs_sh
+  nf_conntrack
+  br_netfilter
+    )
+  rm -f /etc/modules-load.d/ipvs.conf
+  for kernel_module in ${module[@]};do
+      /sbin/modinfo -F filename $kernel_module |& grep -qv ERROR && modprobe -- $kernel_module && echo $kernel_module >> /etc/modules-load.d/ipvs.conf || :
+  done
+
+  systemctl daemon-reload
+  systemctl enable --now systemd-modules-load.service
+
+  # add this if systemd-modules-load report a error
+#   cat>>/usr/lib/systemd/system/systemd-modules-load.service<<EOF
+# [Install]
+# WantedBy=multi-user.target
+# EOF
+
+}
+
+load_k8s_sysctl() {
+  cat << EOF | tee /etc/sysctl.d/k8s.conf
+# 需要禁止ipv6可以打开
+# net.ipv6.conf.all.disable_ipv6 = 1
+# net.ipv6.conf.default.disable_ipv6 = 1
+# net.ipv6.conf.lo.disable_ipv6 = 1
+
+net.ipv4.neigh.default.gc_stale_time = 120
+
+# ubuntu20.04默认是2 calico需要是1
+net.ipv4.conf.all.rp_filter = 1
+net.ipv4.conf.default.rp_filter = 1
+
+net.ipv4.conf.default.arp_announce = 2
+net.ipv4.conf.lo.arp_announce = 2
+net.ipv4.conf.all.arp_announce = 2
+net.ipv4.ip_forward = 1
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_max_syn_backlog = 1024
+net.ipv4.tcp_synack_retries = 2
+
+# 要求iptables不对bridge的数据进行处理
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-arptables = 1
+
+net.netfilter.nf_conntrack_max = 2310720
+fs.inotify.max_user_watches=89100
+fs.file-max = 52706963
+fs.nr_open = 52706963
+vm.overcommit_memory=1
+vm.panic_on_oom=0
+
+vm.swappiness=0
+EOF
+  sysctl -p /etc/sysctl.d/k8s.conf
+  sysctl --system
+}
+
+
+set_localtime
+
+apt_install_base_util
 #remove_docker
-#apt_install_docker
+apt_install_docker
 apt_install_kubeadm_kubectl
+
+disable_swap
+load_module_rke_checks
+load_module_ipvs
+load_k8s_sysctl
+
